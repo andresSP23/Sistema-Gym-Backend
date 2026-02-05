196bf6ba993e2d10b5e9f8fd4e7047ce48faafc0
Andres Serrano <andressukit@hotmail.com>
2026-02-04 20:14:56 -0500

    fix: Actualizar URL de producción en OpenAPI config

--- src/main/java/com/ansicode/SistemaAdministracionGym/config/OpenApiConfig.java
+++ src/main/java/com/ansicode/SistemaAdministracionGym/config/OpenApiConfig.java
@@ -10,53 +10,21 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(
-  info = @Info(
-    contact = @Contact(
-      name = "Andres",
-      email = "andres.dx23@hotmail.com",
-      url = "https://ansicode.com"
+@OpenAPIDefinition(info = @Info(contact = @Contact(name = "Andres", email = "andres.dx23@hotmail.com", url = "https://ansicode.com"
 
-    ),
-    description = "OpenApi documentation for spring security",
-    title = "OpenApi specification  - Ansicode",
-    version = "1.0",
-    license = @License(
-      name = "License name",
-      url = "https://ansicode.com"
+), description = "OpenApi documentation for spring security", title = "OpenApi specification  - Ansicode", version = "1.0", license = @License(name = "License name", url = "https://ansicode.com"
 
+), termsOfService = "Terms of service"
 
-    ),
-    termsOfService = "Terms of service"
-
-  ),
-  servers = {
-    @Server(
-      description = "Local ENV",
-      url = "http://localhost:8081/api/v1"
-    ),
-    @Server(
-      description = "PROD ENV",
-      url = "https://ansicode.com" //fake
-
-    )
-
-  },
-  security = {
-    @SecurityRequirement(
-      name = "bearerAuth"
-    )
-  }
+), servers = {
+    @Server(description = "Local ENV", url = "http://localhost:8081/api/v1"),
+    @Server(description = "PROD ENV (Railway)", url = "https://sistema-gym-backend-production.up.railway.app/api/v1")
+}, security = {
+    @SecurityRequirement(name = "bearerAuth")
+}
 
 )
-@SecurityScheme(
-  name = "bearerAuth",
-  description = "JWT auth description",
-  scheme = "bearer",
-  type = SecuritySchemeType.HTTP,
-  bearerFormat = "JWT",
-  in = SecuritySchemeIn.HEADER
-
+@SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER
 
 )
 public class OpenApiConfig {

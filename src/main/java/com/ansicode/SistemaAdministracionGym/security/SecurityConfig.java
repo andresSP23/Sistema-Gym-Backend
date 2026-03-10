e621c60e03a048bbec0975a319e5e09f6620c651
Andres Serrano <andressukit@hotmail.com>
2026-03-10 17:06:37 -0500

    fix: corregir errores

--- src/main/java/com/ansicode/SistemaAdministracionGym/security/SecurityConfig.java
+++ src/main/java/com/ansicode/SistemaAdministracionGym/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                                 .csrf(AbstractHttpConfigurer::disable)
                                 .authorizeHttpRequests(req -> req.requestMatchers(
                                                 "/auth/**",
+                                                "/actuator/**",
                                                 "/v2/api-docs",
                                                 "/v3/api-docs",
                                                 "/v3/api-docs/**",
